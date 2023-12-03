1e8ef2b94369f553924a470fb013b67f9256b961
austinsaugrich <asaugrich19@gmail.com>
2023-12-02 23:28:57 -0600

    basic 13

---
+++ Web_Fund/basic13/test.js
@@ -0,0 +1,95 @@
+// the basic 13 
+
+
+// print 1-255
+
+// for(let i=0; i <= 255; i++){
+//     console.log(i)
+// }
+
+
+// print ints and sums 0-255
+
+// sum=0
+// for(let i = 0; i<= 255; i++){
+//     sum += i
+// }
+//  console.log(sum)
+
+// print out odds 1-255
+
+// for(let i = 0; i <= 255; i++){
+//     if(i % 2 !== 0)
+//     console.log(i)
+// }
+
+// iterate and print array
+// let array1=["one", "two", "three", "four", "five"]
+
+// function yuh(arr){
+//     for(let i=0; i<=arr.length; i++){
+//     }
+//     console.log(arr)
+// }
+// yuh(array1)
+
+
+// print out max value
+
+// let array2=[1, 2, 3, 4, 5, 72, 6, 7, 8, 9, 10]
+
+//     function max(arr){
+//         let nums = 0
+//         for(let i=0; i<=arr.length; i++){
+//             if(arr[i] > nums)
+//             nums = arr[i]
+//     }
+//     console.log(nums)
+    
+//     }
+//     max(array2)
+
+// print out an average of an array
+
+
+// let array2=[1, 2, 3, 4, 5, 72, 6, 7, 8, 9, 10]
+
+//     function average(arr){
+//         let avg = 0
+//         let sum = 0
+//         for(let i = 0; i<arr.length; i++){
+//             sum += arr[i]
+//         }
+//         avg = sum / arr.length
+//         console.log(avg)
+//     }
+//     average(array2)
+
+//create an array with all the odd intergers between 1 and 255
+
+// function allodds(){
+//     let odds = []
+//     for(let i = 0; i<=255; i++){
+//         if(i % 2 !== 0)
+//         odds.push(i)
+//     }
+//     console.log(odds)
+// }
+// allodds()
+
+//square all the values
+
+// let array2=[1, 2, 3, 4, 5, 72, 6, 7, 8, 9, 10]
+
+// function squareArrayVals(arr){
+//     let nums = 0
+//     let square =[]
+//     for(let i=0; i<arr.length; i++){
+//         nums = arr[i] ** 2
+//         square.push(nums)
+//     }
+//     console.log(square)
+// }
+// squareArrayVals(array2)
+
+
