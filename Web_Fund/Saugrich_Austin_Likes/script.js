86c2cc3b222d83556e8fa188a537d650fe48e889
austinsaugrich <asaugrich19@gmail.com>
2023-12-07 22:40:06 -0600

    proj likes done

---
+++ Web_Fund/Saugrich_Austin_Likes/script.js
@@ -0,0 +1,15 @@
+let neillikes = document.querySelector('#neil');
+let niclikes = document.querySelector('#nic');
+let jimlikes = document.querySelector('#jim');
+
+function nlikes() {
+    neillikes.innerText++;
+}
+
+function nilikes() {
+    niclikes.innerText++;
+}
+
+function jlikes() {
+    jimlikes.innerText++;
+}
