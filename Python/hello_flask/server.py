d92dd44a5c9736d0edf9795f3639781f0a45d395
austinsaugrich <asaugrich19@gmail.com>
2024-01-25 23:50:54 -0600

    playground assignment done

--- Python/hello_flask/server.py
+++ Python/hello_flask/server.py
@@ -1,22 +1,21 @@
-from flask import Flask  # Import Flask to allow us to create our app
-# Create a new instance of the Flask class called "app"
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# The "@" decorator associates this route with the function immediately following
-@app.route('/')
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
-
-# import statements, maybe some other routes
+@app.route('/play')
+def show_box():
+    return render_template('index.html')
 
 
-@app.route('/success')
-def success():
-    return "success"
-
-    # app.run(debug=True) should be the very last statement!
+@app.route('/play/<int:num>')
+def num_of_boxes(num):
+    return render_template('index.html', num=num)
 
 
-if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
-    app.run(debug=True)    # Run the app in debug mode.
+@app.route('/play/<int:num>/<color>')
+def color_of_boxes(color, num):
+    return render_template('index.html', num=num, color=color)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
